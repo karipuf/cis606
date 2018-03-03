04a93622b2d8ebdcabb1cb379df625db69e633d9
Wei Lee Woon <wlwoon@gmail.com>
2018-03-04 02:44:05 +0400

    Added cifar visualization

--- keras_cifar.py
+++ keras_cifar.py
@@ -3,9 +3,10 @@
 
 # In[ ]:
 
+import numpy as np
 from keras import datasets
 from keras.models import Model,Sequential
-from keras.layers import Dense,Conv2D,Activation,Input,Convolution2D,Flatten,BatchNormalization
+from keras.layers import Dropout,Dense,Conv2D,Activation,Input,Convolution2D,Flatten,BatchNormalization
 from keras.utils import np_utils
 from keras.optimizers import Adam
 
@@ -28,15 +29,15 @@ xtest=xtest.astype('float32')/255.
 # Creating classification network
 inp=Input(shape=(32,32,3))
 l1=Conv2D(32,(3,3),padding='valid',strides=(1,1))(inp)
-l1=Activation('relu')(BatchNormalization()(l1))
+l1=Dropout(.2)(Activation('relu')(BatchNormalization()(l1)))
 l2=Conv2D(64,(3,3),padding='valid',strides=(1,1))(l1)
-l2=Activation('relu')(BatchNormalization()(l2))
+l2=Dropout(.2)(Activation('relu')(BatchNormalization()(l2)))
 l3=Conv2D(128,(3,3),padding='valid',strides=(1,1))(l2)
-l3=Activation('relu')(BatchNormalization()(l3))
+l3=Dropout(.2)(Activation('relu')(BatchNormalization()(l3)))
 l4=Conv2D(128,(3,3),padding='valid',strides=(2,2))(l3)
-l4=Activation('relu')(BatchNormalization()(l4))
+l4=Dropout(.2)(Activation('relu')(BatchNormalization()(l4)))
 l5=Conv2D(128,(3,3),padding='valid',strides=(1,1))(l4)
-l5=Activation('relu')(BatchNormalization()(l5))
+l5=Dropout(.2)(Activation('relu')(BatchNormalization()(l5)))
 flat=Flatten()(l3)
 pred=Dense(10,activation='softmax')(flat)
 mod=Model(inputs=inp,outputs=pred)
@@ -47,5 +48,5 @@ mod.compile(Adam(lr=.0001),'categorical_crossentropy',['accuracy'])
 
 # Training
 #mod.fit(xtrain,ytrain)
-mod.fit(xtrain,ytrain,epochs=5,batch_size=128)
-
+mod.fit(xtrain,ytrain,epochs=20,batch_size=128)
+print(np.sum(np.sum(np.argmax(ytest,axis=1)==np.argmax(mod.predict(xtest),axis=1))))
---
+++ visualize_conv.py
@@ -0,0 +1,56 @@
+import keras
+import keras.backend as K
+import numpy as np
+import pylab as pl
+
+mod=keras.models.load_model('model1.hdf')
+
+# Inits
+outLayer=9
+n_iter=500
+eta=.8
+
+# Loss function
+ims=[]
+for filtN in range(9):
+
+    
+    f=K.function([mod.layers[0].input],[K.mean(mod.layers[outLayer].output[:,:,:,filtN])])
+    grad=K.gradients(f.outputs[0],mod.layers[0].input)[0]
+    df=K.function([mod.layers[0].input],[grad])
+    im=np.random.rand(1,32,32,3)
+
+    for count in range(n_iter):
+
+        if count%500==0:
+            print("Updating iteration #"+str(count)+", loss is "+str(f([im])[0]))
+
+        update=df([im])[0]
+        #update=update/(np.sqrt(np.mean(np.square(update)))+.01)
+    
+        im=im+eta*update
+        im=(im-im.min())/(im.max()-im.min())
+
+    ims.append(im[0])
+
+for count in range(9):
+    pl.subplot(3,3,count+1)
+    pl.imshow(ims[count])
+
+
+# vgg
+
+if False:
+    vgg=keras.applications.VGG16()
+    df=K.function([vgg.layers[0].input],K.gradients(K.max(vgg.layers[4].output),vgg.layers[0].input))
+    f=K.function([vgg.layers[0].input],[K.max(vgg.layers[4].output)]) # loss
+
+    for count in range(1000):
+        if count%500==0:
+            print("Updating iteration #"+str(count)+", loss is "+str(f([im])[0]))
+        update=df([im])[0]
+        update=update/(np.sqrt(np.mean(np.square(update)))+.01)
+        
+        im=im+.1*update
+        im=keras.applications.vgg16.preprocess_input(im)
+  
