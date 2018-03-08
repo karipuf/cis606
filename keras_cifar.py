16476c39b6dc3f1a7298a4f4cc5a8f6354a872fb
Wei Lee Woon <wlwoon@gmail.com>
2018-03-08 18:52:41 +0400

    Changed to function form of keras_cifar.py

--- keras_cifar.py
+++ keras_cifar.py
@@ -6,47 +6,43 @@
 import numpy as np
 from keras import datasets
 from keras.models import Model,Sequential
-from keras.layers import Dropout,Dense,Conv2D,Activation,Input,Convolution2D,Flatten,BatchNormalization
+from keras.layers import Dropout,Dense,Conv2D,Activation,Input,Convolution2D,Flatten,BatchNormalization,GlobalAveragePooling2D,Softmax
 from keras.utils import np_utils
 from keras.optimizers import Adam
 
 
-# In[57]:
+# Creating classification network with GAP output
+def CreateCNN(nUnits=(32,64,128,128,128,256),inShape=(None,None,3),dropProb=.2,nClasses=10,linearOut=False):
+    
+    inp=Input(shape=inShape)
+    tens=inp
+    
+    for n in nUnits:
+        tens=Conv2D(n,(3,3),padding='valid',strides=(1,1))(tens)
+        tens=Dropout(dropProb)(Activation('relu')(BatchNormalization()(tens)))
+        
+    tens=Conv2D(nClasses,(1,1))(tens)
+    tens=GlobalAveragePooling2D()(tens)
+    pred=Softmax()(tens)
 
-[(xtrain,ytrain),(xtest,ytest)]=datasets.cifar10.load_data()
+    #flat=Flatten()(l3)
+    #pred=Dense(10,activation='softmax')(flat)
+
+    mod=Model(inputs=inp,outputs=pred)
+    mod.compile(Adam(lr=.0001),'categorical_crossentropy',['accuracy'])
+
+    return mod
 
 
-# In[58]:
-
+# Prepping the data
+[(xtrain,ytrain),(xtest,ytest)]=datasets.cifar10.load_data()    
 ytrain=np_utils.to_categorical(ytrain)
 ytest=np_utils.to_categorical(ytest)
 xtrain=xtrain.astype('float32')/255.
 xtest=xtest.astype('float32')/255.
 
-
-# In[50]:
-
-# Creating classification network
-inp=Input(shape=(None,None,3))
-l1=Conv2D(32,(3,3),padding='valid',strides=(1,1))(inp)
-l1=Dropout(.2)(Activation('relu')(BatchNormalization()(l1)))
-l2=Conv2D(64,(3,3),padding='valid',strides=(1,1))(l1)
-l2=Dropout(.2)(Activation('relu')(BatchNormalization()(l2)))
-l3=Conv2D(128,(3,3),padding='valid',strides=(1,1))(l2)
-l3=Dropout(.2)(Activation('relu')(BatchNormalization()(l3)))
-l4=Conv2D(128,(3,3),padding='valid',strides=(2,2))(l3)
-l4=Dropout(.2)(Activation('relu')(BatchNormalization()(l4)))
-l5=Conv2D(128,(3,3),padding='valid',strides=(1,1))(l4)
-l5=Dropout(.2)(Activation('relu')(BatchNormalization()(l5)))
-flat=Flatten()(l5)
-pred=Dense(10,activation='softmax')(flat)
-mod=Model(inputs=inp,outputs=pred)
-mod.compile(Adam(lr=.0001),'categorical_crossentropy',['accuracy'])
-
-
-# In[55]:
-
-# Training
-#mod.fit(xtrain,ytrain)
+# Creating model and training
+mod=CreateCNN()
 mod.fit(xtrain,ytrain,epochs=20,batch_size=128)
+
 print(np.sum(np.sum(np.argmax(ytest,axis=1)==np.argmax(mod.predict(xtest),axis=1))))
